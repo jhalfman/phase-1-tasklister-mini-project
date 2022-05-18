a5f48c74f34d2d6b40418bb7fbf67e3318517856
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-17 19:43:01 -0700

    my project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  //grab task form
+  const taskElement = document.getElementById("create-task-form");
+
+  //grab list
+  const theList = document.getElementById("tasks");
+
+  //event listener for form
+  taskElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //add item to list\
+    const listElement = document.createElement("li");
+    theList.appendChild(listElement);
+    listElement.innerHTML = e.target["new-task-description"].value;
+    e.target.reset();
+  })
 });
